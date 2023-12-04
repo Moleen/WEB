6295d3d5d85976054bde6f0e3473de97def3144a
Moleen <maulana.syakhiya@gmail.com>
2023-12-04 21:04:37 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&family=PT+Sans:ital,wght@1,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style/index.css">  
+    <title>Document</title>
+</head>
+<body>
+
+    <?php include ('src/navbar.php') ?>
+
+    <div class="about">
+        <div class="greet">
+            <h1>Hello!, i am Maulana</h1>
+            <p>i am a junior web developer</p>
+        </div>
+    </div>
+
+    <div class="Contact">
+
+    </div>
+</body>
+</html>
---
+++ src/navbar.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="../style/navbar.css">
+<header>
+    <nav>
+        <div class="logo"><a href="">Maulana<span>SK</span></a></div>
+        <ul>
+            <li><a href="">About</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+    </nav>
+</header>
